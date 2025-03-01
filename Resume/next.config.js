ba2e8a5fb030b6a361d34bf1ca7ccf660f0d9dde
Nalinnipha <66022736@up.ac.th>
2025-03-01 23:00:39 +0700

    ลบ

--- Resume/next.config.js
+++
@@ -1,7 +0,0 @@
-// next.config.js
-module.exports = {
-    output: 'export',
-    basePath: '/my-resume', // เปลี่ยนเป็นชื่อ repo ของคุณ
-    assetPrefix: '/my-resume', // เปลี่ยนเป็นชื่อ repo ของคุณ
-  }
-  
